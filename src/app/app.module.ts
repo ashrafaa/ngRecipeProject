8e4ead139f26affd035061845a7fd71dfa666f95
ashraf_anwar <ashraf.a@spartadigital.co.uk>
2017-12-09 01:38:25 +0000

    Added components skeleton

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,24 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipeBookComponent } from './recipe-book/recipe-book.component';
+import { RecipeDetailComponent } from './recipe-book/detail/detail.component';
+import { RecipeItemComponent } from './recipe-book/item/item.component';
+import { RecipeListComponent } from './recipe-book/list/list.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/edit/edit.component';
 
-
-@NgModule({
+@NgModule({ 
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipeBookComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    RecipeListComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule
