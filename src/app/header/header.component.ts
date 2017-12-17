2fe489ebb48b9882f6a4b1843124569732dce968
SD MacBookAir 1 <>
2017-12-17 23:47:12 +0000

    Added property and event binding to link between pages

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,8 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
 export class HeaderComponent {
+  @Output() page = new EventEmitter<string>();
+
+  onLinkSelect(link: string) {
+    this.page.emit(link);
+  }
 }
