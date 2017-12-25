a3768d01525a53d22c50acaa365a3038688e1f09
Ashraf Anwar <ashrafanwar@Spartas-MacBook-Pro.local>
2017-12-25 18:46:58 +0000

    Added navigation through routerLink and set routerLinkActive

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,13 +1,8 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
 export class HeaderComponent {
-  @Output() page = new EventEmitter<string>();
-
-  onLinkSelect(link: string) {
-    this.page.emit(link);
-  }
 }
